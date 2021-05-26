317015de416198e049121c1df2a96341f5973a7b
redwing555 <75939922+redwing555@users.noreply.github.com>
2021-05-26 18:11:37 +0200

    complicated pagination and individual author posts page

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Post
 
+
 admin.site.register(Post)
+
--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,7 +3,8 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
                     )
 from . import views
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
     path('post/new/', PostCreateView.as_view(), name = 'post-create'),
+    path('user/str:<username>', UserPostListView.as_view(), name = 'user-posts'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name = 'post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name = 'post-delete'),
     path('about/', views.about, name = 'blog-about'),
